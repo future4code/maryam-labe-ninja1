6eed733ed7a4415d399ee7317fba4c80ec77cfc2
Fabio Faria <fabiofadev87@gmail.com>
2021-09-27 18:22:59 -0300

    Revert "Adicionado paginas"
    This reverts commit 41c00824fe2167a1a7c977bb965bb8c3b8f2e635.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import React from "react";
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import BeANinja from "./components/BeANinja/BeANinja";
-import FindANinja from "./components/FindANinja/FindANinja";
+import React from 'react'
+import { AppContainer } from './components/AppContainer'
 
-class App extends React.Component {
-  state = {
-    pageContent: "home",
-  };
-
-  renderPage = () => {
-    if (this.state.currentPage === "BeANinja") {
-      return <BeANinja onChangePage={this.changePage} />;
-    } else if (this.state.currentPage === "FindANinja") {
-      return <FindANinja onChangePage={this.changePage} />;
-    } else if (this.state.currentPage === "home") {
-      return <Home />;
-    }
-    return <Home onChangePage={this.changePage} />;
-  };
-
-  changePage = (page) => {
-    this.setState({
-      currentPage: page,
-    });
-  };
-  render() {
-    return (
-      <div onChangePage={this.changePage}>
-        <Header />
-        {this.renderPage()}
-      </div>
-    );
-  }
+function App() {
+	return (
+        <AppContainer />
+	)
 }
 
-export default App;
+export default App
--- src/components/FindANinja/FindANinja.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class FindANinja extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default FindANinja;
